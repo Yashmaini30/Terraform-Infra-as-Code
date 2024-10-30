614b28a21305b758b4ce27d86c1a6f354d2d894b
Yash Maini <mainiyash2@gmail.com>
2024-10-30 18:54:48 +0530

    made consul for environment

---
+++ 07-managing-multiple-env/consul/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket         = "practice-devops-tf-state"
+    key            = "06-web-app/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+}
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "consul" {
+  source = "git@github.com:hashicorp/terraform-aws-consul.git"
+}
