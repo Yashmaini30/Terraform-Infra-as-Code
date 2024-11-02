f757a010db79cea2a15473a6db1301179afe0d93
Yash Maini <mainiyash2@gmail.com>
2024-11-02 16:13:56 +0530

    made production dir

---
+++ 08-managing-multiple-env/file-structure/production/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  backend "s3" {
+    bucket = "practice-devops-tf-state"
+    key    = "08-managing-multiple-env/production/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "db_pass" {
+  description = "password for database"
+  type = string
+  sensitive = true
+}
+
+locals {
+  environment_name = "production"
+}
+
+module "web_app" {
+  source = "../../../07-organization-and-modules/web-app-module"
+
+  # Input Variables
+  bucket_prefix = "web-app-data-${local.environment_name}"
+  domain = "mainiyash.com"
+  environment_name = local.environment_name
+  instance_type = "t2.micro"
+  create_dns_zone  = false
+  db_name          = "${local.environment_name}mydb"
+  db_user          = "foo"
+  db_pass          = var.db_pass
+}
